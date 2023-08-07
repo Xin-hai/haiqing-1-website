c68100194f4c3f1b862f43c0ab3bdce27a79241c
Xin-hai <aa1940358283@163.com>
2023-08-07 11:50:04 +0800

    deploy

---
+++ precache-manifest.188e5f1e4844efc3e3a1584e151b7c88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb1329a076ebf0e1731",
+    "url": "/haiqing-1-website/css/app.caebaddd.css"
+  },
+  {
+    "revision": "a157a1dde8f26264ab2d",
+    "url": "/haiqing-1-website/css/chunk-vendors.06c3b195.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/haiqing-1-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/haiqing-1-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ea1c2baa9f5aabbfacf572f415ec84bf",
+    "url": "/haiqing-1-website/img/crying.ea1c2baa.jpg"
+  },
+  {
+    "revision": "43453d7910255ebbceff2965118a4645",
+    "url": "/haiqing-1-website/index.html"
+  },
+  {
+    "revision": "2eb1329a076ebf0e1731",
+    "url": "/haiqing-1-website/js/app.4ff9f35a.js"
+  },
+  {
+    "revision": "a157a1dde8f26264ab2d",
+    "url": "/haiqing-1-website/js/chunk-vendors.e4cf9fdf.js"
+  },
+  {
+    "revision": "5104eaf4345fbc5919f50df9a0edea99",
+    "url": "/haiqing-1-website/manifest.json"
+  },
+  {
+    "revision": "8ae2308d596424f03b280346bb107323",
+    "url": "/haiqing-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/haiqing-1-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/haiqing-1-website/precache-manifest.188e5f1e4844efc3e3a1584e151b7c88.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "haiqing-1"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
